b45f21003f68affbadc4358d3c5799da376c37b3
yousufjoy <yousufj93006@gmail.com>
2025-03-30 12:46:44 +0600

    update

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -7,25 +7,19 @@ function Team() {
       id: 1,
       name: "Tanveer Ahmed",
       role: "UI/UX Artist",
-      imgSrc: "/assets/1.jpg",  // Correct file names, and update other srcs
-      linkedinUrl: "#", // Add actual LinkedIn URLs if available
-      twitterUrl: "#"   // Add actual Twitter URLs if available
+      imgSrc: "/assets/T_3.jfif"
     },
     {
       id: 2,
       name: "Md Sifat",
-      role: "Owner & CEO | Programmer | Level Designer | 3D Artist", // Shorten the role for better layout
-      imgSrc: "/assets/1.jpg", 
-      linkedinUrl: "#",
-      twitterUrl: "#"
+      role: "Owner & CEO | Programmer | Level Designer | 3D Artist",
+      imgSrc: "/assets/3e435c08-935a-47cf-a2b3-467c603cddef.jpg"
     },
     {
       id: 3,
       name: "Rashid Naive",
       role: "3D Hard Surface and Product Modeler",
-      imgSrc: "/assets/1.jpg", 
-      linkedinUrl: "#",
-      twitterUrl: "#"
+      imgSrc: "/assets/T_2.jfif"
     }
   ];
 
@@ -37,16 +31,14 @@ function Team() {
           <span className="block w-20 h-1 bg-purple-500 absolute bottom-0 left-1/2 transform -translate-x-1/2 mt-4"></span>
         </h2>
 
-        {/* Modified grid for better responsiveness and spacing */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-6">
+        {/* Adjusted grid layout for larger cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {teamMembers.map(member => (
             <TeamMember
               key={member.id}
               name={member.name}
               role={member.role}
               imgSrc={member.imgSrc}
-              linkedinUrl={member.linkedinUrl}  // Pass down social media URLs
-              twitterUrl={member.twitterUrl}
             />
           ))}
         </div>
